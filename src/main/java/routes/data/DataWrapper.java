c0e7f8dc8cb45493934fe698b76fa1187e0aafb4
akrpv <al.kurapov@gmail.com>
2018-10-27 19:29:56 +0300

    created method getPlacesByCategoryId() in class db

--- src/main/java/routes/data/DataWrapper.java
+++ src/main/java/routes/data/DataWrapper.java
@@ -29,10 +29,40 @@ public class DataWrapper {
         System.out.println("Соединения закрыты");
     }
 
+
+
+
+
+    public static ArrayList<Place> getPlacesByCategoryId(long categoryId) throws  ClassNotFoundException, SQLException {
+        ArrayList<Place> PlaceList = new ArrayList<>();
+
+        Connection connection = getConnection();
+
+        PreparedStatement st2 = connection.prepareStatement("SELECT time FROM Categories WHERE id = " + categoryId);
+        ResultSet r2 = st2.executeQuery();
+        int time = 0;
+        while(r2.next()){
+
+             time = r2.getInt(1);
+        }
+        PreparedStatement st = connection.prepareStatement("SELECT Category_id,x,y,name FROM Places WHERE Category_id = " + categoryId);
+        ResultSet r1 = st.executeQuery();
+
+        while(r1.next()){
+
+            PlaceList.add(new Place(r1.getLong("Category_id"), r1.getDouble("x"),r1.getDouble("y"),r1.getString("name"),time));
+        }
+
+        closeDB(connection);
+        return PlaceList;
+    }
+
     public static ArrayList<Category> getCategories() throws ClassNotFoundException, SQLException {
 
         ArrayList<Category> listOfCategories = new ArrayList<>();
         Connection connection = getConnection();
+
+
         PreparedStatement st = connection.prepareStatement("SELECT * FROM Categories ");
         ResultSet r1 = st.executeQuery();
 
--- src/main/java/routes/data/Place.java
+++ src/main/java/routes/data/Place.java
@@ -2,19 +2,21 @@ package routes.data;
 
 public class Place {
 
+
     private double x;
     private double y;
     private String name;
     private long categoryId;
     private int time; //in minutes
 
-    public Place(double x, double y, String name, long categoryId, int time) {
+    public Place(long categoryId, double x, double y, String name, int time) {
         this.x = x;
         this.y = y;
         this.name = name;
         this.categoryId = categoryId;
         this.time = time;
     }
+
 
     public double getX() {
         return x;
@@ -27,6 +29,7 @@ public class Place {
     public String getName() {
         return name;
     }
+
 
     public long getCategoryId() {
         return categoryId;
