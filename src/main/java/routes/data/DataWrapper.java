221e26848ee4d0b4b6413f1ec3ac594d9d3eb038
Alexander <dr.sanches3@gmail.com>
2018-10-27 20:06:58 +0300

    getPlacesByCategoryId modified

--- src/main/java/routes/data/DataWrapper.java
+++ src/main/java/routes/data/DataWrapper.java
@@ -1,12 +1,10 @@
 package routes.data;
 
-
 import java.sql.*;
 import java.util.ArrayList;
-
+import java.util.List;
 
 public class DataWrapper {
-
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
 
@@ -20,36 +18,28 @@ public class DataWrapper {
         return conn;
     }
 
-
-
     public static void closeDB(Connection conn) throws  SQLException {
         conn.close();
-
-
         System.out.println("Соединения закрыты");
     }
 
-
-
-
-
-    public static ArrayList<Place> getPlacesByCategoryId(long categoryId) throws  ClassNotFoundException, SQLException {
+    public static List<Place> getPlacesByCategoryId(long categoryId, double x, double y, double r) throws  ClassNotFoundException, SQLException {
         ArrayList<Place> PlaceList = new ArrayList<>();
-
         Connection connection = getConnection();
 
         PreparedStatement st2 = connection.prepareStatement("SELECT time FROM Categories WHERE id = " + categoryId);
         ResultSet r2 = st2.executeQuery();
         int time = 0;
         while(r2.next()){
-
              time = r2.getInt(1);
         }
-        PreparedStatement st = connection.prepareStatement("SELECT Category_id,x,y,name FROM Places WHERE Category_id = " + categoryId);
+
+        String query = String.format("SELECT Category_id, x, y, name FROM Places WHERE Category_id = %d AND ((x - %f) * (x - %f) + (y - %f) * (y - %f) <= %f * %f)",
+                categoryId, x, x, y, y, r, r);
+        PreparedStatement st = connection.prepareStatement(query);
         ResultSet r1 = st.executeQuery();
 
         while(r1.next()){
-
             PlaceList.add(new Place(r1.getLong("Category_id"), r1.getDouble("x"),r1.getDouble("y"),r1.getString("name"),time));
         }
 
